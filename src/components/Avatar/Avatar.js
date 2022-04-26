614dcc7586185643c7f1d0554b51efd62294254d
Shirataitel <perki.tayytel100@gmail.com>
2022-04-26 12:29:00 +0300

    o

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -3,9 +3,8 @@ import React from 'react'
 const Avatar = ({profileSRC}) => {
   return (
     <img
-    src={profileSRC ?? 'https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg'}
-    alt="profile"
-    />
+    src={profileSRC || 'https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg'}
+    alt="profile"/>
   )
 }
 
